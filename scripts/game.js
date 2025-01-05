d209d4d8e6a348629713f2cce36040717763478f
May Pattaranan <56375291+maysunktong@users.noreply.github.com>
2025-01-05 15:27:02 +0100

    Implement full gravity for player

--- scripts/game.js
+++ scripts/game.js
@@ -4,6 +4,7 @@ const ctx = canvas.getContext("2d");
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
+const gravity = 1;
 class Player {
   constructor() {
     this.position = {
@@ -15,7 +16,7 @@ class Player {
 
     this.velocity = {
       x: 0,
-      y: 1,
+      y: 0,
     };
   }
 
@@ -25,8 +26,12 @@ class Player {
   }
 
   update() {
-    this.position.y += this.velocity.y;
     this.draw();
+    this.position.y += this.velocity.y;
+
+    if (this.position.y + this.height + this.velocity.y <= canvas.height)
+      this.velocity.y += gravity;
+    else this.velocity.y = 0;
   }
 }
 
@@ -34,6 +39,9 @@ let player = new Player();
 
 function animate() {
   requestAnimationFrame(animate);
+  ctx.fillStyle = "white";
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
   player.update();
 }
 
