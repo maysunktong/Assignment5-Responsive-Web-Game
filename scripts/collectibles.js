8fed1942bac568315bc4a94f050f1b84be3ae66c
May Pattaranan <56375291+maysunktong@users.noreply.github.com>
2025-01-05 01:51:12 +0100

    Import and export files

---
+++ scripts/collectibles.js
@@ -0,0 +1,15 @@
+import collectibleCoins from "../assets/sprites/collectibles/coins.png";
+import collectibleDiamonds from "../assets/sprites/collectibles/diamonds.png";
+import collectibleGreenRunes from "../assets/sprites/collectibles/greenRunes.png";
+import collectibleRedRunes from "../assets/sprites/collectibles/redRunes.png";
+import collectibleRuby from "../assets/sprites/collectibles/ruby.png";
+import collectibleStars from "../assets/sprites/collectibles/stars.png";
+
+export const collectibles = {
+  stars: collectibleStars,
+  coins: collectibleCoins,
+  diamonds: collectibleDiamonds,
+  greenRunes: collectibleGreenRunes,
+  redRunes: collectibleRedRunes,
+  ruby: collectibleRuby,
+};
---
+++ scripts/sprites.js
@@ -0,0 +1,85 @@
+import swordsManIdleRight from "../assets/sprites/Swordsman/Idle.png";
+import swordsManIdleLeft from "../assets/sprites/Swordsman/left/Idle_left.png";
+import swordsManRunLeft from "../assets/sprites/Swordsman/left/Run_left.png";
+import swordsManRunRight from "../assets/sprites/Swordsman/Run.png";
+
+import wizardIdleRight from "../assets/sprites/Wizard/Idle.png";
+import wizardIdleLeft from "../assets/sprites/Wizard/left/Idle_left.png";
+import wizardRunLeft from "../assets/sprites/Wizard/left/Run_left.png";
+import wizardRunRight from "../assets/sprites/Wizard/Run.png";
+
+import musketeerIdleRight from "../assets/sprites/Musketeer/Idle.png";
+import musketeerIdleLeft from "../assets/sprites/Musketeer/left/Idle_left.png";
+import musketeerRunLeft from "../assets/sprites/Musketeer/left/Run_left.png";
+import musketeerRunRight from "../assets/sprites/Musketeer/Run.png";
+
+import archerIdleRight from "../assets/sprites/Archer/Idle.png";
+import archerIdleLeft from "../assets/sprites/Archer/left/Idle_left.png";
+import archerRunLeft from "../assets/sprites/Archer/left/Run_left.png";
+import archerRunRight from "../assets/sprites/Archer/Run.png";
+
+import blackWolfWalkLeft from "../assets/sprites/Werewolf/walk_left.png";
+import blackWolfWalkRight from "../assets/sprites/Werewolf/walk_right.png";
+
+import redWolfRunLeft from "../assets/sprites/Werewolf/run_left.png";
+import redWolfRunRight from "../assets/sprites/Werewolf/run_right.png";
+
+import whiteWolfAttackLeft from "../assets/sprites/Werewolf/attack_left.png";
+import whiteWolfAttackRight from "../assets/sprites/Werewolf/attack_right.png";
+
+export const sprites = {
+  swordsman: {
+    idle: {
+      right: swordsManIdleRight,
+      left: swordsManIdleLeft,
+    },
+    run: {
+      right: swordsManRunRight,
+      left: swordsManRunLeft,
+    },
+  },
+  wizard: {
+    idle: {
+      right: wizardIdleRight,
+      left: wizardIdleLeft,
+    },
+    run: {
+      right: wizardRunRight,
+      left: wizardRunLeft,
+    },
+  },
+  musketeer: {
+    idle: {
+      right: musketeerIdleRight,
+      left: musketeerIdleLeft,
+    },
+    run: {
+      right: musketeerRunRight,
+      left: musketeerRunLeft,
+    },
+  },
+  archer: {
+    idle: {
+      right: archerIdleRight,
+      left: archerIdleLeft,
+    },
+    run: {
+      right: archerRunRight,
+      left: archerRunLeft,
+    },
+  },
+  werewolf: {
+    walk: {
+      right: blackWolfWalkRight,
+      left: blackWolfWalkLeft,
+    },
+    run: {
+      right: redWolfRunRight,
+      left: redWolfRunLeft,
+    },
+    attack: {
+      right: whiteWolfAttackRight,
+      left: whiteWolfAttackLeft,
+    },
+  },
+};
