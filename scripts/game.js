012d8f658e41df3ec89b0d5ab7a1637da9f0ba8a
May Pattaranan <56375291+maysunktong@users.noreply.github.com>
2025-01-05 15:22:09 +0100

    Add velocity starter, player is drawn downward

--- scripts/game.js
+++ scripts/game.js
@@ -12,14 +12,29 @@ class Player {
     };
     this.width = 50;
     this.height = 50;
+
+    this.velocity = {
+      x: 0,
+      y: 1,
+    };
   }
 
   draw() {
     ctx.fillStyle = "green";
     ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
-    
+  }
+
+  update() {
+    this.position.y += this.velocity.y;
+    this.draw();
   }
 }
 
 let player = new Player();
-player.draw();
+
+function animate() {
+  requestAnimationFrame(animate);
+  player.update();
+}
+
+animate();
