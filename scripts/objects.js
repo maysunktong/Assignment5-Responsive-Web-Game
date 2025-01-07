06a78cee42e0242c1fd2f96a21d235894534f2b5
May Pattaranan <56375291+maysunktong@users.noreply.github.com>
2025-01-07 03:00:35 +0100

    Refactor objects with function

--- scripts/objects.js
+++ scripts/objects.js
@@ -1,3 +1,5 @@
+import { createImage } from "./utils";
+
 import collectibleCoins from "../assets/sprites/collectibles/coins.png";
 import collectibleDiamonds from "../assets/sprites/collectibles/diamonds.png";
 import collectibleGreenRunes from "../assets/sprites/collectibles/greenRunes.png";
@@ -7,11 +9,11 @@ import collectibleStars from "../assets/sprites/collectibles/stars.png";
 
 export const objects = {
   collectibles: {
-    stars: collectibleStars,
-    coins: collectibleCoins,
-    diamonds: collectibleDiamonds,
-    greenRunes: collectibleGreenRunes,
-    redRunes: collectibleRedRunes,
-    ruby: collectibleRuby,
+    stars: createImage(collectibleStars),
+    coins: createImage(collectibleCoins),
+    diamonds: createImage(collectibleDiamonds),
+    greenRunes: createImage(collectibleGreenRunes),
+    redRunes: createImage(collectibleRedRunes),
+    ruby: createImage(collectibleRuby),
   },
 };
